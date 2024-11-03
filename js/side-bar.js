f54f1a1935682ead3ebcbcce01d7258bfc0efc8c
Eliando Gomes <eliandro.gomes42@gmail.com>
2024-11-03 20:54:43 -0300

    Adiciona a estrutura base dos menus da barra lateral com animações

--- js/side-bar.js
+++ js/side-bar.js
@@ -1,6 +1,27 @@
-const userBar = document.querySelector("#user-bar")
-const user = document.querySelector(".user-menu")
+const sideBar = document.querySelector(".itens")
+const closeButton = document.querySelector(".close-button")
+const user = document.querySelector(".menu")
+const userMenu = document.querySelector(".user")
+const configMenu = document.querySelector(".config")
 
-userBar.addEventListener('click', () => {
-    user.classList.toggle('active');
+closeButton.addEventListener("click", () => {
+    user.classList.remove("active")
+})
+
+sideBar.addEventListener("click", function(event) {
+    const item = event.target.closest('.item');
+    if (item && item.id !== "exit" && item.id !== "open-bar") {
+        user.classList.add("active");
+        removeClassLists()
+        if (item.id === "user-menu") {
+            userMenu.classList.toggle("active")
+        } else if (item.id === "config-menu") {
+            configMenu.classList.toggle("active")
+        }
+    }   
 });
+
+function removeClassLists() {
+    userMenu.classList.remove("active")
+    configMenu.classList.remove("active")
+};
