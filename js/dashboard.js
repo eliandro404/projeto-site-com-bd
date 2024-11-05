b9f8436e47e206bdf352776c16d72277eb8bf1ed
Eliando Gomes <eliandro.gomes42@gmail.com>
2024-11-05 17:16:31 -0300

    Finaliza ajustes finais na animação dos menus

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,8 +2,10 @@ const sidebar = document.querySelector(".barra-lateral")
 const item = document.querySelector(".itens")
 const texto = document.querySelectorAll(".item-texto")
 const botaoSidebar = document.querySelector(".btn-alternar")
+const menu = document.querySelector(".menu")
 
 botaoSidebar.addEventListener('click', ()=> {
+    menu.classList.toggle('l')
     sidebar.classList.toggle('active');
     item.classList.toggle('active');
     texto.forEach(element => {
