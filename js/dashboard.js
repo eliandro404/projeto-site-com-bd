1837b3067d2b1910b0e1fc2c8106f609095d8645
Eliando Gomes <eliandro.gomes42@gmail.com>
2024-11-30 04:17:51 -0300

    Altera visual dos botões do contêiner da direita e adciona animações a ele

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,18 +1,38 @@
 const fullscrenBtn = document.getElementById("expand-button")
 const sideBar = document.getElementById("sidebar-buttons")
-const botao = document.getElementById("settings-menu")
 const appPage = document.querySelector(".app-page")
-let paddingApplied = false;
 const sideBarMenu = document.querySelector(".sidebar-menu-body")
+const tabButtons = document.querySelectorAll(".tab-button")
+const active = document.querySelector(".active")
+let paddingApplied = false;
 
-botao.addEventListener("click", () => {
-    sideBarMenu.classList.toggle("showing")
-    if (paddingApplied) {
-        appPage.style.paddingLeft = "0px";
-    } else {
-        appPage.style.paddingLeft = "250px";
+sideBar.addEventListener("click", (event) => {
+    const button = event.target.closest(".sidebar-button");
+    const buttons = document.querySelectorAll(".sidebar-button")
+
+    // buttons.forEach(appPage.classList.remove("showing"))
+
+    if (button.id === "profile-menu" || button.id === "help-menu" || button.id === "settings-menu") {
+        sideBarMenu.classList.add("showing")
+        if (paddingApplied) {
+            appPage.style.paddingLeft = "0px";
+        } else {
+            appPage.style.paddingLeft = "250px";
+        }
+        paddingApplied = !paddingApplied
     }
-    paddingApplied = !paddingApplied
+})
+
+tabButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        if (event.target.id === "buyNSell") {
+            active.style.left = "3%"
+        } else if (event.target.id === "extra1") {
+            active.style.left = "35%"
+        } else if (event.target.id === "extra2") {
+            active.style.left = "67%"
+        }
+    })
 })
 
 fullscrenBtn.addEventListener("click", () => {
@@ -21,24 +41,4 @@ fullscrenBtn.addEventListener("click", () => {
     } else {
         document.documentElement.requestFullscreen()
     }
-})
-
-
-
-
-
-const sidebar = document.querySelector(".barra-lateral")
-const item = document.querySelector(".itens")
-const texto = document.querySelectorAll(".item-texto")
-const botaoSidebar = document.querySelector(".btn-alternar")
-const menu = document.querySelector(".menu")
-
-botaoSidebar.addEventListener('click', ()=> {
-    menu.classList.toggle('l')
-    sidebar.classList.toggle('active');
-    item.classList.toggle('active');
-    texto.forEach(element => {
-        element.classList.toggle('active');
-    });
-    botaoSidebar.classList.toggle('active');
 })
